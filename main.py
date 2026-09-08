daa0add193ed806946e0c70358fad0d04e409f53
nolegs123 <92927899+nolegs123@users.noreply.github.com>
2026-09-08 16:03:37 +0200

    Refactor experiment settings and mode selection

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ def get_available_words():
 # RUN NUMBER
 # =========================
 
-def get_next_run_number():
+def get_next_run_number(mode):
     if not os.path.exists(CSV_FILE):
         return 1
 
@@ -158,6 +158,9 @@ def get_next_run_number():
 
         for row in reader:
             try:
+                if row["mode"] != mode["name"]:
+                    continue
+
                 run_number = int(row["run"])
 
                 highest_run = max(
@@ -604,7 +607,6 @@ def save_results(
                     math_total
             })
 
-        # Force the results to be written to disk immediately
         file.flush()
         os.fsync(file.fileno())
 
@@ -667,7 +669,6 @@ def run_experiment(
         math_total
     )
 
-    # Save immediately after this run is completed
     save_results(
         run_number,
         mode,
@@ -677,7 +678,8 @@ def run_experiment(
     )
 
     print(
-        f"\nRun {run_number} saved to {CSV_FILE}."
+        f"\nRun {run_number} "
+        f"saved to {CSV_FILE}."
     )
 
 
@@ -702,7 +704,7 @@ def main():
             break
 
         start_run = (
-            get_next_run_number()
+            get_next_run_number(mode)
         )
 
         for run_offset in range(
